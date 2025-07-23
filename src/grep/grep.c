7579d4fbbd9d8a32b88cc29f79d2c1dcf68e4a3a
renminh <216414881+renminh@users.noreply.github.com>
2025-07-23 19:38:29 -0400

    grep: allow grep to do multiple files

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -6,14 +6,12 @@
 bool is_word_in_line(const char *line, const char *word) {
     size_t line_len = strlen(line);
     size_t word_len = strlen(word);
+    int n = 0;
+    bool found_word = false;
 
     if (line_len < word_len)
         return false;
 
-    int n = 0;
-    // char ch;
-
-    bool found_word = false;
 
     while (n < (line_len - word_len)) {
         
@@ -21,12 +19,12 @@ bool is_word_in_line(const char *line, const char *word) {
         // starting from n to n + word_len
         // using j as the index to the word, and i as the index
         // to the line
+        found_word = true;
         for (int i = n, j = 0; j < word_len; i++, j++) {
             if (line[i] != word[j]) {
                 found_word = false;
                 break;
             }
-            found_word = true;
         }
 
         if (found_word)
@@ -44,8 +42,8 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "grep: searchterm [file ...]\n");
         exit(1);
     }
+    
     char *word = strdup(argv[1]);
-
     char *buffer;
     size_t bufsize = 255;
     size_t characters;
@@ -55,7 +53,8 @@ int main(int argc, char *argv[]) {
         perror("malloc");
         exit(1);
     }
-
+    
+    // if no file was given for the args
     if (argc == 2) {
         while ((characters = getline(&buffer, &bufsize, stdin)) != -1) {
             if (is_word_in_line(buffer, word))
@@ -69,7 +68,7 @@ int main(int argc, char *argv[]) {
         
         if (file == NULL) {
             fprintf(stderr, "grep: %s: file does not exist\n", argv[i]);
-            exit(1);
+            continue;
         }
 
         while ((characters = getline(&buffer, &bufsize, file)) != -1) {
@@ -79,7 +78,8 @@ int main(int argc, char *argv[]) {
         
         fclose(file);
     }
-
+    
+    free(word);
     free(buffer);
 
     return 0;
