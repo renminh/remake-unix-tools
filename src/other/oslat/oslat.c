408b73be6cd4fa69a0c0247d9b33649ed42d3be7
renminh <216414881+renminh@users.noreply.github.com>
2025-07-24 00:11:33 -0400

    oslat: add context switch benchmark and accurate delta calculation

--- src/other/oslat/oslat.c
+++ src/other/oslat/oslat.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+
+#define NUM_SYSCALLS 1000
 
 /*
  * oslat means os latency for 
@@ -14,32 +19,79 @@
  * context switch
  */
 
+static inline uint64_t get_delta(const uint64_t start_s, const uint64_t end_s,
+                   const uint64_t start_ns, const uint64_t end_ns) {
+    return (end_s - start_s) * 1e9 + (end_ns - start_ns);
+}
+
 int main(int argc, char **argv) {
     struct timespec ts;
-    char buff[0];
-    size_t nbyte = 0;
-    
-    size_t sum_nsec = 0;
-    int num_syscalls;
+    uint64_t sum_ns = 0;
 
-    printf("How many syscalls do you wish to run: ");
-    scanf("%d", &num_syscalls);
+    printf("RUNNING %d SYSCALLS FOR BENCHMARKING:\n",
+           NUM_SYSCALLS);
     
-    for (int i = 0; i < num_syscalls; i++) {
-        size_t start_nsec, end_nsec;
+    for (int i = 0; i < NUM_SYSCALLS; i++) {
+        uint64_t start_s, end_s;
+        uint64_t start_ns, end_ns;
 
         clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-        start_nsec = (size_t) ts.tv_nsec;
-        // read(STDIN_FILENO, buff, nbyte);
+        start_s = (uint64_t) ts.tv_sec;
+        start_ns = (uint64_t) ts.tv_nsec;
         getpid();
         clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
-        end_nsec = (size_t) ts.tv_nsec;
-
-        sum_nsec += end_nsec - start_nsec;
+        end_s = (uint64_t) ts.tv_sec;
+        end_ns = (uint64_t) ts.tv_nsec;
+        
+        sum_ns += get_delta(start_s, end_s, start_ns, end_ns);
     }
 
-    printf("average of %0.2f nanoseconds per %d syscalls\n",
-           (float) sum_nsec / num_syscalls, num_syscalls);
+    float avg_ns_syscalls = (float) sum_ns / NUM_SYSCALLS;
+
+    printf("Average system call time:\t%0.2fns\n", avg_ns_syscalls);
+
+    /*
+     *
+     * for context switching, we can use pipes
+     * via ping pong back and forth some nth amount
+     *
+     */
+
+    // todo, add ping pong for pipes
+    // search how
+
+    int fd[2];
+
+    if (pipe(fd) < 0) {
+        perror("pipe");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    } else if (pid == 0) {
+        fprintf(stdin, "nothing\n");
+    } else {
+        uint64_t start_s, end_s;
+        uint64_t start_ns, end_ns;
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+        start_s = (uint64_t) ts.tv_sec;
+        start_ns = (uint64_t) ts.tv_nsec;
+
+        pid_t rc_wait = wait(NULL);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+        end_s = (uint) ts.tv_sec;
+        end_ns = (uint) ts.tv_nsec;
+
+        sum_ns = get_delta(start_s, end_s, start_ns, end_ns);
+        
+        printf("Average context switch time:\t%0.2fμs\n",
+               ((float) sum_ns - avg_ns_syscalls) / 1000);
+    }
     
     return 0;
 }
